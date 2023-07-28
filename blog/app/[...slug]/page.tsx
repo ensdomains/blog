2ec682d6447d8c2e770cc5f955316bbd07ab29ac
Luc <luc@lucemans.nl>
2023-07-28 22:41:21 +0000

    Introduce OpenGraph

--- blog/app/[...slug]/page.tsx
+++ blog/app/[...slug]/page.tsx
@@ -1,13 +1,49 @@
 import { MDXProps } from 'mdx/types';
+import { ResolvingMetadata } from 'next';
 import { JSX } from 'react';
 
 import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeader } from '@/components/PostHeader';
 import { getPostBySlug } from '@/lib/get_post_by_slug';
 import { getPostsMetadata } from '@/lib/get_posts';
+import { createMetadata } from '@/lib/metadata';
 
 type PageProperties = {
     params: { slug: string[] };
+};
+
+export const generateMetadata = async (
+    { params }: PageProperties,
+    parent: ResolvingMetadata
+) => {
+    if (params.slug.length !== 1)
+        throw new Error('Invalid slug ' + params.slug.join('/'));
+
+    const post = await getPostBySlug(params.slug[0]);
+    const parentMetadata = await parent;
+
+    return createMetadata(
+        {
+            title: post.title,
+            description: post.description,
+            path: params.slug.join('/'),
+        },
+        parentMetadata,
+        {
+            openGraph: {
+                type: 'article',
+                title: post.title,
+                authors: post.authors?.map((author) => author),
+                images: post.cover,
+                description: post.description,
+                tags: post.tags,
+            },
+            authors: post.authors?.map((author) => ({
+                name: author,
+                url: `https://ens.app/${author}`,
+            })),
+        }
+    );
 };
 
 // eslint-disable-next-line unicorn/prevent-abbreviations
