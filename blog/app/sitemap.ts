36f8735e43cc9301c8ad10791a9e736000f0c300
Luc <luc@lucemans.nl>
2023-07-29 00:37:03 +0000

    Introduce Sitemap, Robots.txt, & Favicon

---
+++ blog/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next';
+
+import { getPostsMetadata } from '@/lib/get_posts';
+
+const BASE_URL = 'https://blog.ens.build/';
+
+const sitemap = async () => {
+    const posts = await getPostsMetadata();
+
+    return posts.map((post) => ({
+        url: BASE_URL + post.slug,
+        lastModified: post.date ?? new Date(),
+    })) as MetadataRoute.Sitemap;
+};
+
+export default sitemap;
