6931b379a55db4303275c39eb715b2e37af46848
Svemat <jakob.helgesson@gmail.com>
2023-08-08 21:25:40 +0000

    Apply to h3 aswell and remove old

--- blog/mdx/rehype.mjs
+++ blog/mdx/rehype.mjs
@@ -64,7 +64,10 @@ function rehypeSlugify() {
         const slugify = slugifyWithCounter();
 
         visit(tree, 'element', (node) => {
-            if (node.tagName === 'h2' && !node.properties.id) {
+            if (
+                (node.tagName === 'h2' || node.tagName === 'h3') &&
+                !node.properties.id
+            ) {
                 node.properties.id = slugify(toString(node));
             }
         });
--- blog/src/components/mdx/heading/index.tsx
+++ blog/src/components/mdx/heading/index.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { FC, PropsWithChildren } from 'react';
 
 import { cx } from '@/lib/cx';
