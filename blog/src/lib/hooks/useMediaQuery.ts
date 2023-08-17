c5421ef7af12b2d126ca0e5c44ef6a7bca6cfa8e
Svemat <jakob.helgesson@gmail.com>
2023-08-17 07:52:02 +0000

    Introduce useMediaQuery hook

---
+++ blog/src/lib/hooks/useMediaQuery.ts
@@ -0,0 +1,44 @@
+// Source: https://usehooks-ts.com/react-hook/use-media-query
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query: string): boolean {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const getMatches = (query: string): boolean => {
+        // Prevents SSR issues
+        if (typeof window !== 'undefined') {
+            return window.matchMedia(query).matches;
+        }
+
+        return false;
+    };
+
+    const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+    function handleChange() {
+        setMatches(getMatches(query));
+    }
+
+    useEffect(() => {
+        const matchMedia = window.matchMedia(query);
+
+        // Triggered at the first client-side load and if query changes
+        handleChange();
+
+        // Listen matchMedia
+        if (matchMedia.addListener) {
+            matchMedia.addListener(handleChange);
+        } else {
+            matchMedia.addEventListener('change', handleChange);
+        }
+
+        return () => {
+            if (matchMedia.removeListener) {
+                matchMedia.removeListener(handleChange);
+            } else {
+                matchMedia.removeEventListener('change', handleChange);
+            }
+        };
+    }, [query]);
+
+    return matches;
+}
