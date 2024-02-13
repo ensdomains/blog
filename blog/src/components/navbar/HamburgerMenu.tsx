8cd25be1ae18e53869aebe1acf1cb425ed163919
Jonatan Holmgren <jonatan@jontes.page>
2024-02-13 09:21:56 +0100

    Use React-Icons for X

--- blog/src/components/navbar/HamburgerMenu.tsx
+++ blog/src/components/navbar/HamburgerMenu.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { FC } from 'react';
 import { FaDiscord, FaYoutube } from 'react-icons/fa';
 import { FiGithub, FiMessageCircle } from 'react-icons/fi';
+import { RiTwitterXFill } from 'react-icons/ri';
 
 import { CrossSVG, MenuSVG } from '../ClientIcons';
 
@@ -17,14 +18,7 @@ const links = [
 
 const socials = [
     {
-        icon: (
-            <div className="h-5 w-5 fill-neutral-400 transition-colors hover:fill-black">
-                {/* X.com logo */}
-                <svg viewBox="0 0 1200 1227" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
-                </svg>
-            </div>
-        ),
+        icon: <RiTwitterXFill />,
         to: 'https://twitter.com/ensdomains',
     },
     { icon: <FiGithub />, to: 'https://github.com/ensdomains' },
