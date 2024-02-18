c9512678b560965d0e1682350d685dac3ccc14d8
Luc <luc@lucemans.nl>
2024-02-18 19:53:30 +0000

    Update Analytics

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                     data-domain="blog.ens.domains"
                     src="https://plausible.io/js/script.js"
                 ></script>
+                <script
+                    defer
+                    data-domain="blog.ens.domains"
+                    src="https://ens.v3x.report/js/script.js"
+                ></script>
                 <script defer src="https://v3x.report/please.js"></script>
             </body>
         </html>
